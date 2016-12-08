5ea4dc19c054cd0c8a2bb3dba3e946ec42e9dffa
v.adhithyan <aavispeaks@gmail.com>
2016-12-08 09:58:52 +0530

    Go to Main Activity after SplashTimeOut

--- app/src/main/java/ceg/avtechlabs/kandhasastikavasam/activities/SplashScreenActivity.java
+++ app/src/main/java/ceg/avtechlabs/kandhasastikavasam/activities/SplashScreenActivity.java
@@ -1,5 +1,7 @@
 package ceg.avtechlabs.kandhasastikavasam.activities;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
@@ -8,6 +10,8 @@ import android.view.WindowManager;
 import ceg.avtechlabs.kandhasastikavasam.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+  final int SPLASH_TIME_OUT = 2000;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +22,15 @@ public class SplashScreenActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
 
     setContentView(R.layout.activity_splash_screen);
+
+    new Handler().postDelayed(new Runnable() {
+      @Override
+      public void run() {
+        Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+        startActivity(intent);
+      }
+    }, SPLASH_TIME_OUT);
+
   }
+
 }
