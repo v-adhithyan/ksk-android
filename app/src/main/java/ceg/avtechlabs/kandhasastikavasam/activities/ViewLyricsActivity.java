b550657be6aee31604532747dcb58956f1f8e8ec
v.adhithyan <aavispeaks@gmail.com>
2016-12-08 09:47:26 +0530

    :lipstick: layout files changed and supporting changes

---
+++ app/src/main/java/ceg/avtechlabs/kandhasastikavasam/activities/ViewLyricsActivity.java
@@ -0,0 +1,15 @@
+package ceg.avtechlabs.kandhasastikavasam.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ceg.avtechlabs.kandhasastikavasam.R;
+
+public class ViewLyricsActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_lyrics_options);
+  }
+}
